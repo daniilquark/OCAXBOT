4de15457eb6acf96206351ad387fbc801efc5062
daniilquark <daniilquark@gmail.com>
2025-03-22 21:39:47 +0000

    Added mapping from server.js

--- routes/webhook.js
+++ routes/webhook.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const client = require('../db/client');
+const webhookToMessages = require('./webhookToMessages');
+const checkAndNotifyHashtag = require('./checkAndNotifyHashtag');
 
 router.post('/tgwh', async (req, res) => {
   console.log("Output data", req.body);
@@ -17,15 +19,18 @@ router.post('/tgwh', async (req, res) => {
   const values = [JSON.stringify(req.body)];
 
   try {
-    await client.query(createTableQuery); // Создаём таблицу, если её нет
-    await client.query(insertQuery, values); // Сохраняем JSON
+    await client.query(createTableQuery);
+    await client.query(insertQuery, values);
+
+    await webhookToMessages(); // Обновляем messages на основании data
+    await checkAndNotifyHashtag(); // Проверяем наличие хештега test на основании text of messages
 
     res.status(200).json({
       message: "Data received and saved",
       receivedData: req.body
     });
   } catch (err) {
-    console.error('❌ Error saving data:', err.stack);
+    console.error('Error saving data:', err.stack);
     res.status(500).json({
       message: 'Error saving data to the database'
     });
