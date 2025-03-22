828277679cf80c51fc37390dd223d90547b708df
daniilquark <daniilquark@gmail.com>
2025-03-22 17:44:43 +0000

    Added logic for mapping webhook, slice to connect db and routes

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const fs = require('fs');
 const https = require('https');
 const express = require('express');
 const bodyParser = require('body-parser');
-const { Client } = require('pg');
 require('dotenv').config();
 
 const app = express();
@@ -13,40 +12,17 @@ const options = {
   cert: fs.readFileSync('/etc/letsencrypt/live/daniilquark.com/fullchain.pem')
 };
 
-const client = new Client({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
-});
-
-client.connect()
-  .then(() => {
-    console.log('Connected to the database');
-  })
-  .catch(err => {
-    console.error('Database connection error:', err.stack);
-});
-
+// Middlewares
 app.use(bodyParser.json());
 
-app.post('/tgwh', (req, res) => {
-    console.log("Output data", req.body);
+// Routes
+const webhookRoutes = require('./routes/webhook');
+app.use('/', webhookRoutes);
 
-    const query = 'INSERT INTO data(data) VALUES($1)';
-    const values = [JSON.stringify(req.body)];
+// Mappings
+require('./routes/webhookToMessages');
 
-    client.query(query, values)
-        .then(() => {
-            res.status(200).json({ message: "Data received and saved", receivedData: req.body });
-        })
-        .catch(err => {
-            console.error('Error saving data:', err.stack);
-            res.status(500).json({ message: 'Error saving data to the database' });
-    });
-});
-
+// HTTPS server
 https.createServer(options, app).listen(port, '0.0.0.0', () => {
   console.log(`by https://daniilquark.com:${port}`);
 });
