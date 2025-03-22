ab0db43efde8830737b9dc9ddecfdc00c2795a83
daniilquark <daniilquark@gmail.com>
2025-03-22 21:40:55 +0000

    Corrected function about collumns in db

--- routes/webhookToMessages.js
+++ routes/webhookToMessages.js
@@ -9,9 +9,10 @@ async function webhookToMessages() {
         date TIMESTAMP,
         username TEXT,
         text TEXT,
+        reaction_by_hashtag BOOLEAN DEFAULT FALSE,
         message_id INTEGER,
-        reply_chat_id BIGINT,
-        reply_chat_title TEXT,
+        chat_id BIGINT,
+        chat_title TEXT,
         reply_thread_id INTEGER,
         reply_topic_name TEXT
       );
@@ -25,8 +26,8 @@ async function webhookToMessages() {
         username,
         text,
         message_id,
-        reply_chat_id,
-        reply_chat_title,
+        chat_id,
+        chat_title,
         reply_thread_id,
         reply_topic_name
       )
@@ -36,8 +37,8 @@ async function webhookToMessages() {
         data->'message'->'from'->>'username',
         data->'message'->>'text',
         (data->'message'->>'message_id')::INTEGER,
-        (data->'message'->'reply_to_message'->'chat'->>'id')::BIGINT,
-        data->'message'->'reply_to_message'->'chat'->>'title',
+        (data->'message'->'chat'->>'id')::BIGINT,
+        data->'message'->'chat'->>'title',
         (data->'message'->'reply_to_message'->>'message_thread_id')::INTEGER,
         data->'message'->'reply_to_message'->'forum_topic_created'->>'name'
       FROM data
@@ -51,10 +52,5 @@ async function webhookToMessages() {
     console.error('Ошибка при вставке webhook-данных из таблицы data в таблицу messages:', err);
   }
 }
-
-// Периодический запуск (если нужно)
-setInterval(() => {
-  webhookToMessages();
-}, 5000);
 
 module.exports = webhookToMessages;
